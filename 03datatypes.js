a84d9690e1985874b508413be20d5274b4298b1b
thecodeanish <thecode.anish@gmail.com>
2024-01-27 16:05:43 +0530

    This is datatypes learning file

---
+++ 03datatypes.js
@@ -0,0 +1,10 @@
+// There are some datatypes use in JS
+//Number => 3 range - 2 to power 53
+// bigint
+//String => " "
+// boolean => true/false
+// null => It is standarlon value. It is used where we do not want to 0 and undefined, means for empaty value
+// undefined 
+//symbol => unique
+
+//object
