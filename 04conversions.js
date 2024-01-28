a41940b134a46868748d14a1dd7bcd6f2a53f76b
thecodeanish <thecode.anish@gmail.com>
2024-01-28 23:51:16 +0530

    Learn some new concepts of js

--- 04conversions.js
+++ 04conversions.js
@@ -1,1 +1,35 @@
 // Learning Conversions
+let a=30;
+console.log(a);
+console.log(typeof(a));
+
+let scope = String(a)
+console.log(scope)
+console.log(typeof(scope))
+
+let b=true;
+console.log(typeof(b));
+
+
+
+/* ==============> Operations <========== */
+let value =20
+let negValue = -value;
+console.log(negValue)
+
+
+// Airthmetic Operations
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2%2)
+console.log(2/2)
+
+let str1 ="Anish"
+let str2 = " Kumar"
+console.log(str3 = str1 + str2)
+
+console.log(true)
+console.log(+true)
